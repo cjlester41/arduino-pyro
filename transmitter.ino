12a4b05925829ee0095d5d2936e26584f1fe57dc
cjlester41 <149527318+cjlester41@users.noreply.github.com>
2026-07-21 10:38:11 -0700

    false trigger prevention logic
    Refactor loop to prevent multiple triggers and add a delay before sending messages.

--- transmitter.ino
+++ transmitter.ino
@@ -2,30 +2,34 @@
 
 int pin;
 int fire_delay = 30;
+bool triggered = false;
 SoftwareSerial HC12(10, 11); 
- 
+
 void setup() { 
   for (pin = 4; pin < 8; pin++) {
     digitalWrite(pin, LOW); 
-    pinMode(pin, OUTPUT);      
+    pinMode(pin, OUTPUT);     
   }
   HC12.begin(9600); 
   pinMode(2, INPUT_PULLUP); 
+  while (digitalRead(2) == LOW);
 }
 
 void loop() {    
-  if (digitalRead(2) == LOW) {    
-    for (int i = 0; i < 4; i++) {
-      HC12.print("LETS_FUCKING_GO\n");
-      delay(40); 
+  if (!triggered && digitalRead(2) == LOW) {    
+    delay(50);
+    if (digitalRead(2) == LOW) {
+      triggered = true;
+      for (int i = 0; i < 4; i++) {
+        HC12.print("LETS_FUCKING_GO\n");
+        delay(50); 
+      }      
+      for (pin = 4; pin < 8; pin++) {
+        digitalWrite(pin, HIGH);
+        delay(4 * 1000);
+        digitalWrite(pin, LOW);
+        delay((fire_delay - 4) * 1000);    
+      }
     }
-    
-    for (pin = 4; pin < 8; pin++) {
-      digitalWrite(pin, HIGH);
-      delay(4 * 1000);
-      digitalWrite(pin, LOW);
-      delay((fire_delay - 4) * 1000);    
-    }
-    exit(0);
   }
 }
